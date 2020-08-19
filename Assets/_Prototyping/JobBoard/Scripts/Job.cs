9a51b3060b877a04913ddde0946cba811132d0d0
Matthew Murphy <mdmurphy2@wisc.edu>
2020-08-19 14:59:29 -0500

    Began Prototyping JobBoard added functionality to create a list of buttons

---
+++ Assets/_Prototyping/JobBoard/Scripts/Job.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProtoAqua.JobBoard
+{
+    
+
+    public class Job : MonoBehaviour
+    {
+        
+
+        public enum JobName {
+            Job1,
+            Job2,
+            Job3,
+            Job4
+        }
+
+        public static int getJobReward(JobName jobName) {
+            switch(jobName) {
+                default:
+                case JobName.Job1: return 1000;
+                case JobName.Job2: return 100;
+                case JobName.Job3: return 5000;
+                case JobName.Job4: return 1;
+            }
+        } 
+
+        //Will need sprites
+        //Difficulty
+        //ID?
+        //Description
+        
+    }
+
+}
---
+++ Assets/_Prototyping/JobBoard/Scripts/JobBoard.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace ProtoAqua.JobBoard
+{
+    public class JobBoard : MonoBehaviour
+    {
+        public Sprite defaultSprite;
+        private Transform container;
+        private Transform jobButtonTemplate;
+
+        private void Awake() {
+            container = transform.Find("jobList");
+            jobButtonTemplate = container.Find("jobButtonTemplate");
+            jobButtonTemplate.gameObject.SetActive(false);
+        }
+
+        private void Start() {
+
+            //Create The jobs (Will loop through sometihng later)
+            CreateJobButton("The First Job",Job.getJobReward(Job.JobName.Job1), 1);
+            CreateJobButton("The Second Job",Job.getJobReward(Job.JobName.Job2), 2);
+            CreateJobButton("The Third Job",Job.getJobReward(Job.JobName.Job2), 3);
+
+            Debug.Log("test");
+        }
+
+         public void SelectJob(string jobName) {
+            Debug.Log("test button " + jobName);
+        }
+
+        
+
+        //Creates a job button by replicating the "template" and creating a duplicate
+        private void CreateJobButton(string jobName, int jobReward, int positionIndex) {
+            Transform jobButtonTransform = Instantiate(jobButtonTemplate, container);
+            RectTransform jobButtonRectTransform = jobButtonTemplate.GetComponent<RectTransform>();
+
+            float jobButtonHeight = 80f;
+            jobButtonRectTransform.anchoredPosition = new Vector2(0, -jobButtonHeight * positionIndex);
+
+            jobButtonTransform.Find("jobName").GetComponent<TextMeshProUGUI>().SetText(jobName);
+            jobButtonTransform.Find("jobReward").GetComponent<TextMeshProUGUI>().SetText(jobReward.ToString());
+            
+            jobButtonTransform.gameObject.SetActive(true);
+
+
+
+            //jobButtonTransform.Find("jobPicture").GetComponent<Image>.sprite = defaultSprite;
+
+            jobButtonTransform.GetComponent<Button>().onClick.AddListener(() => SelectJob(jobName));
+
+        
+        }
+
+
+    }
+}
