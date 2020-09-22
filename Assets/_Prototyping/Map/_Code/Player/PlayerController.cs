e46ed2e53ed39312d8abb3a6ae57004c3a6787db
mdmurphy2 <43936034+mdmurphy2@users.noreply.github.com>
2020-09-22 11:03:38 -0500

    Changed to perspective camera, added functionality to move character around while clicking on screen

--- Assets/_Prototyping/Map/_Code/Player/PlayerController.cs
+++ Assets/_Prototyping/Map/_Code/Player/PlayerController.cs
@@ -7,15 +7,16 @@ namespace ProtoAqua.Map {
     
     public class PlayerController : MonoBehaviour {
 
-        [SerializeField] Rigidbody2D playerRigidBody;
-        [SerializeField] float moveSpeed = 100f;
+        [SerializeField] Rigidbody2D playerRigidBody = null;
+        [SerializeField] float moveSpeed = .5f;
+        [SerializeField] Camera mainCamera = null;
 
     
 
         private Vector3 mousePosition;
         private Vector2 direction;
 
-
+        
 
 
         
@@ -24,19 +25,35 @@ namespace ProtoAqua.Map {
 
         }
 
-
         // Update is called once per frame
-        void Update() {
+        void FixedUpdate() {
+
+
             if(Input.GetMouseButton(0)) {
-                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition = ScreenToWorldOnPlane(Input.mousePosition, transform);
+
                 direction = (mousePosition - transform.position).normalized;
                 playerRigidBody.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
-                //vs
 
             }
             else {
                 playerRigidBody.velocity = Vector2.zero;
             }
+
+            
+        }
+
+        public Vector3 ScreenToWorldOnPlane(Vector2 inScreenPos, Transform inWorldRef) {
+            Vector3 screenPos = inScreenPos;
+            screenPos.z = 1;
+
+            Plane p = new Plane(-mainCamera.transform.forward, inWorldRef.position);
+            Ray r = mainCamera.ScreenPointToRay(screenPos);
+
+            float dist;
+            p.Raycast(r, out dist);
+
+            return r.GetPoint(dist);
         }
 
         
