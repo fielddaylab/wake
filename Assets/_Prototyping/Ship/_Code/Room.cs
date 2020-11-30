9ebf7e30bc05ed67310af5ad502698f7586e952e
Matthew Murphy <mdmurphy2@wisc.edu>
2020-11-30 10:39:40 -0600

    Updated the Ship Scene to not be a canvas, and transition to other parts when clicking helm

---
+++ Assets/_Prototyping/Ship/_Code/Room.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using BeauRoutine;
+
+namespace ProtoAqua.Ship
+{
+    public class Room : MonoBehaviour
+    {
+        [Header("Room Declarations")]
+        [SerializeField] string m_RoomId = null;
+        [SerializeField] string m_RoomText = null;
+        [SerializeField] string m_SceneToLoad = null;
+
+        [Header("Room Dependencies")]
+        [SerializeField] Camera m_MainCamera = null;
+        [SerializeField] TextMeshProUGUI m_TextBox = null;
+        [SerializeField] CanvasGroup m_CanvasGroup = null;
+        [SerializeField] Transform m_RoomTransform = null;
+
+        private Routine fadeRoutine;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //m_TextBox.text = m_RoomText;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void RoomClick()
+        {
+            
+            //SceneManager.LoadScene(m_SceneToLoad);
+            fadeRoutine.Replace(this, FadeRoutine());
+            Debug.Log("clicked me");
+        }
+        public void OnMouseUpAsButton() { //Pointer Listener
+            RoomClick();
+        }
+
+        private void MoveCameraToRoom() {
+            m_MainCamera.transform.position = new Vector3(m_RoomTransform.position.x, m_RoomTransform.position.y, m_MainCamera.transform.position.z);
+        }
+        private IEnumerator FadeRoutine() {
+            yield return m_CanvasGroup.FadeTo(1f, 1f).Ease(Curve.Smooth);
+            MoveCameraToRoom();
+            yield return m_CanvasGroup.FadeTo(0f, 1f).Ease(Curve.Smooth);
+        }
+        
+
+    }
+}
+
---
+++ Assets/_Prototyping/Ship/_Code/RoomManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProtoAqua.Ship
+{
+    public class RoomManager : MonoBehaviour
+    {
+         public Camera camera;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if (Input.GetMouseButtonDown(0))
+            // {
+            //     Debug.Log("Mouse is down");
+
+            //     RaycastHit hitInfo;
+            //     bool hit = Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            //     if (hit)
+            //     {
+            //         Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+            //         if (hitInfo.transform.gameObject.tag == "Construction")
+            //         {
+            //             Debug.Log("It's working!");
+            //         }
+            //         else
+            //         {
+            //             Debug.Log("nopz");
+            //         }
+            //     }
+            //     else
+            //     {
+            //         Debug.Log("No hit");
+            //     }
+            //     Debug.Log("Mouse is down");
+            // }
+        }
+    }
+}
