ad1c44e3f8c81ed744c35fd80b51d9ba30312701
Forest San Filippo <forest@flipply.com>
2021-02-08 17:07:04 -0600

    Added color scheme swapping example script
    - Created color scheme swapping example script ("ChangeColorScheme.cs")
    - Added script to mini-sub prefabs

---
+++ Assets/_Prototyping/SeaScene/_Code/ChangeColorScheme.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorScheme : MonoBehaviour
+
+{
+
+    public int colorScheme;
+    public float totalSchemes = 16;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float schemeFraction = (1 / totalSchemes)*colorScheme;
+        rend = GetComponent<Renderer>();
+        rend.material.SetTextureOffset("_MainTex", new Vector2(schemeFraction, 0));
+
+    }
+
+}
