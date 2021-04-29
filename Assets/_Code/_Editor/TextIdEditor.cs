dde5f7b1aab3fc78773ecdd3cfa9f6db67f489d0
Autumn <beauchesne@wisc.edu>
2021-04-29 18:26:27 -0500

    * Improved localization key editor * Improved workflow for validating loc keys and opening files to add new entries

--- Assets/_Code/_Editor/TextIdEditor.cs
+++ Assets/_Code/_Editor/TextIdEditor.cs
@@ -1,4 +1,5 @@
 using BeauUtil;
+using BeauUtil.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,22 +8,41 @@ namespace Aqua.Editor
     [CustomPropertyDrawer(typeof(TextId))]
     public class TextIdEditor : PropertyDrawer
     {
-        private const float HashDisplayWidth = 90;
+        [SerializeField] private string m_TempText;
 
-        private GUIStyle m_HashStyle;
+        private const float TextIconDisplayWidth = 45;
+
+        private GUIStyle m_NullIconStyle;
+        private GUIStyle m_FoundIconStyle;
+        private GUIStyle m_MissingIconStyle;
+
+        private GUIContent m_MissingContent;
+        private GUIContent m_ValidContent;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (m_HashStyle == null)
+            if (m_NullIconStyle == null)
             {
-                m_HashStyle = new GUIStyle(EditorStyles.label);
-                m_HashStyle.normal.textColor = Color.gray;
+                m_NullIconStyle = new GUIStyle(EditorStyles.label);
+                m_NullIconStyle.normal.textColor = Color.gray;
+            }
+
+            if (m_FoundIconStyle == null)
+            {
+                m_FoundIconStyle = new GUIStyle(EditorStyles.label);
+                m_FoundIconStyle.normal.textColor = Color.green;
+            }
+
+            if (m_MissingIconStyle == null)
+            {
+                m_MissingIconStyle = new GUIStyle(EditorStyles.label);
+                m_MissingIconStyle.normal.textColor = Color.yellow;
             }
 
             label = EditorGUI.BeginProperty(position, label, property);
             Rect propRect = position;
-            propRect.width -= HashDisplayWidth - 4;
-            Rect labelRect = new Rect(propRect.xMax + 4, propRect.y, HashDisplayWidth, propRect.height);
+            propRect.width -= TextIconDisplayWidth - 4;
+            Rect iconRect = new Rect(propRect.xMax + 4, propRect.y, TextIconDisplayWidth, propRect.height);
             
             EditorGUI.BeginChangeCheck();
             var stringProp = property.FindPropertyRelative("m_Source");
@@ -34,12 +54,39 @@ namespace Aqua.Editor
                 hashProp.longValue = new StringHash32(stringProp.stringValue).HashValue;
             }
             
-            int lastIndent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            EditorGUI.LabelField(labelRect, "0x" + hashProp.longValue.ToString("X8"), m_HashStyle);
-
-            EditorGUI.indentLevel = lastIndent;
+            string key = stringProp.stringValue;
+            if (string.IsNullOrEmpty(key))
+            {
+                using(GUIScopes.IndentLevelScope.SetIndent(0))
+                {
+                    EditorGUI.LabelField(iconRect, "Null", m_NullIconStyle);
+                }
+            }
+            else
+            {
+                string content;
+                if (LocEditor.TryLookup(key, out content))
+                {
+                    using(GUIScopes.IndentLevelScope.SetIndent(0))
+                    {
+                        var guiContent = m_ValidContent ?? (m_ValidContent = new GUIContent("Valid"));
+                        guiContent.tooltip = content;
+                        EditorGUI.LabelField(iconRect, guiContent, m_FoundIconStyle);
+                        m_TempText = content;
+                    }
+                }
+                else
+                {
+                    using(GUIScopes.IndentLevelScope.SetIndent(0))
+                    {
+                        var guiContent = m_MissingContent ?? (m_MissingContent = new GUIContent("Unkn", "Right-click to open file for editing"));
+                        EditorGUI.LabelField(iconRect, guiContent, m_MissingIconStyle);
+                        
+                        if (Event.current.type == EventType.ContextClick && iconRect.Contains(Event.current.mousePosition))
+                            LocEditor.AttemptOpenFile(key);
+                    }
+                }
+            }
 
             UnityEditor.EditorGUI.EndProperty();
         }
