19ad07f959860072b14536fee47dba5a93e77fdb
Travis <tbird3@wisc.edu>
2021-06-15 13:23:30 -0500

    Day/Night cycle shader for sites

---
+++ Assets/_Prototyping/SeaScene/_Code/SiteDayNightShaderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BeauUtil;
+using UnityEngine;
+
+
+namespace Aqua
+{
+    [RequireComponent(typeof(ColorGroup))]
+    public class SiteDayNightShaderController : TimeAnimatedObject
+    {
+        /*private void Start()
+        {
+            Shader.SetGlobalColor("_LightColor", LightPalette.Evening);
+            Shader.SetGlobalColor("_ShadowColor", ShadowPalette.Night);
+        }*/
+
+        #region Inspector
+
+        [Inline(InlineAttribute.DisplayType.HeaderLabel)] public TimeColorPalette LightPalette;
+        [Inline(InlineAttribute.DisplayType.HeaderLabel)] public TimeColorPalette ShadowPalette;
+
+        #endregion // Inspector
+
+        public override void OnTimeChanged(InGameTime inGameTime)
+        {
+            Shader.SetGlobalColor("_LightColor", LightPalette.GetValueForTime(inGameTime));
+            Shader.SetGlobalColor("_ShadowColor", ShadowPalette.GetValueForTime(inGameTime));
+        }
+    }
+}
