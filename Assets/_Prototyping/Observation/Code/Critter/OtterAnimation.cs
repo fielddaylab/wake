3402a62c73b2ed870ffe7a5da976ec85a57ce777
Forest San Filippo <freakyforest@gmail.com>
2021-09-21 14:16:26 -0500

    Updated Otter Animations
    - Updated otter animations and controller with separate idle state
    - Added OtterAnimations script that allows for animation start delay and idle repeats to be set per-instance
    - Added start delay and idle repeat settings to otters in RD-1D-Visuals as
    (Note: I'm sometimes seeing a small animation glitch in the editor - will keep an eye on it)

---
+++ Assets/_Prototyping/Observation/Code/Critter/OtterAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtterAnimation : MonoBehaviour
+{
+    public float startDelay;
+    public int idleRepeat = 1;
+    public Animator animator;
+    private int idleCount = 0;
+
+    void OnEnable()
+    {
+        //animator = GetComponent<Animator>();
+        StartCoroutine(DelayedAnimation());
+    }
+
+    // The delay coroutine
+    IEnumerator DelayedAnimation()
+    {
+        yield return new WaitForSeconds(startDelay);
+        animator.Play("SwimTo");
+    }
+
+    public void idlePlayed()
+    {
+        idleCount = idleCount + 1;
+        Debug.Log("IdleCount:" + idleCount);
+    }
+
+    private void Update()
+    {
+        if (idleRepeat <= idleCount)
+        {
+            animator.SetTrigger("swimAway");
+            idleCount = 0;
+        }
+    }
+}
