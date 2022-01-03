a779434c573e580cc436d5f8eba24bae176e8786
Forest San Filippo <freakyforest@gmail.com>
2022-01-03 15:57:57 -0600

    Added Light Change Script & Updated Player Prefab
    - Added script to change a lights intensity based on the y position of the camera
    - Added mini-sub model, texture and material
    - Added light beam textures for spotlight
    - Added prefab for spot light
    - Integrated mini-sub model and spotlight prefab into player prefab

---
+++ Assets/_Prototyping/Observation/Code/Env/LightingOverDepth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingOverDepth : MonoBehaviour
+{
+    Camera mainCamera;
+    float cameraPos;
+    public Light mainLight;
+    float brightestLightIntensity;
+    public float darkestLightIntensity;
+    public float darkeningStart = 0;
+    public float darkeningEnd = -20;
+
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        if (mainLight != null)
+        {
+            brightestLightIntensity = mainLight.intensity;
+            Debug.Log("Darkest Light Intensity:" + darkestLightIntensity);
+        }
+    }
+
+
+    void Update()
+    {
+        cameraPos = mainCamera.transform.position.y;
+
+        if (cameraPos > darkeningStart)
+        {
+            mainLight.intensity = brightestLightIntensity;
+        }
+        else if (cameraPos > darkeningEnd)
+        {
+            float camPercent = Mathf.InverseLerp(darkeningEnd, darkeningStart, cameraPos);
+            mainLight.intensity = Mathf.Lerp(darkestLightIntensity, brightestLightIntensity, camPercent);
+        }
+
+    }
+}
