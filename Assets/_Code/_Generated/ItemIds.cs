966ac5c19a9957b55ad28dc7e31a8bc12eba51c8
Autumn <beauchesne@wisc.edu>
2022-04-03 00:48:20 -0500

    * Improved output of progression graph data, included version of shop charity intervention * Ensured kelp-bull-kelp-forest unlocks ArcticStation * Removed unused reference to "Argumentation" room in kelp-welcome script * Ensure arctic-whale-csi is appropriately placed at the correct station

--- Assets/_Code/_Generated/ItemIds.cs
+++ Assets/_Code/_Generated/ItemIds.cs
@@ -10,14 +10,11 @@ static public class ItemIds
 	static public readonly StringHash32 ObservationTank = new StringHash32(0x09DE9891);
 	static public readonly StringHash32 StressTank = new StringHash32(0xE5262769);
 	static public readonly StringHash32 WaterStabilizer = new StringHash32(0xC3EF968D);
-	static public readonly StringHash32 AutoTagger = new StringHash32(0x602904D0);
 	static public readonly StringHash32 Engine = new StringHash32(0xDB61155B);
 	static public readonly StringHash32 Flashlight = new StringHash32(0xD184FA33);
-	static public readonly StringHash32 HoneyPot = new StringHash32(0x513267E7);
 	static public readonly StringHash32 Hull = new StringHash32(0xEED0C4D6);
 	static public readonly StringHash32 Icebreaker = new StringHash32(0x38F974D4);
 	static public readonly StringHash32 Microscope = new StringHash32(0x12AA1E11);
-	static public readonly StringHash32 PopulationProbe = new StringHash32(0xD07A2E9C);
 	static public readonly StringHash32 ProbeHacker = new StringHash32(0xD34A2A23);
 	static public readonly StringHash32 PropGuard = new StringHash32(0xEADECBED);
 	static public readonly StringHash32 ROVScanner = new StringHash32(0xBE07D45E);
