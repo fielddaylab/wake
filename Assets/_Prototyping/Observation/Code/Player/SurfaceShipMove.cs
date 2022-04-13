1beebf541f48d85b823cf73ee2a6747690c314a0
Forest San Filippo <freakyforest@gmail.com>
2022-04-13 13:00:51 -0500

    First Pass On New Ship & Surface Navigation
    - Added shader graphs for water surface and foam edges
    - Added materials & textures for foam edges, water surface, wake trails, skybox & ship
    - Added post processing profile for bull kelp surface
    - Added models for ship and misc foam edges
    - Added basic scrips for ship and camera movement (just to get everything kind-of working)
    - Added surface scene with water, ship terrain, skybox etc.

---
+++ Assets/_Prototyping/Observation/Code/Player/SurfaceShipMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceShipMove : MonoBehaviour
+{
+
+    public float shipSpeed = 1.0f;
+    public float shipTurnSpeed = 1.0f;
+    Vector3 newPosition;
+
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float step = shipSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+                //transform.LookAt(newPosition);
+                Quaternion OriginalRot = transform.rotation;
+                transform.LookAt(newPosition);
+                Quaternion NewRot = transform.rotation;
+                transform.rotation = OriginalRot;
+                transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, shipTurnSpeed * Time.deltaTime);
+
+            }
+        }
+
+    }
+}
