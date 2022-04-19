e074002d4ce868b55e48edb0f58e7589fc6fccbb
Forest San Filippo <freakyforest@gmail.com>
2022-04-19 13:56:56 -0500

    Updated Ship Movement & Textures
    - Added more detail to ship texture and updated UVs
    - Updated ship bob animation with more movement
    - Added code to slow ship when mouse is released

--- Assets/_Prototyping/Observation/Code/Player/SurfaceShipMove.cs
+++ Assets/_Prototyping/Observation/Code/Player/SurfaceShipMove.cs
@@ -8,6 +8,8 @@ public class SurfaceShipMove : MonoBehaviour
     public float shipSpeed = 1.0f;
     public float shipTurnSpeed = 1.0f;
     Vector3 newPosition;
+    private bool slowing = false;
+    private float distance = 0.0f;
 
     void Start()
     {
@@ -20,13 +22,13 @@ public class SurfaceShipMove : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
+            slowing = false;
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
                 newPosition = hit.point;
                 transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
-                //transform.LookAt(newPosition);
                 Quaternion OriginalRot = transform.rotation;
                 transform.LookAt(newPosition);
                 Quaternion NewRot = transform.rotation;
@@ -34,6 +36,24 @@ public class SurfaceShipMove : MonoBehaviour
                 transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, shipTurnSpeed * Time.deltaTime);
 
             }
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            slowing = true;
+            distance = Vector3.Distance(transform.position, newPosition);
+        }
+
+        if (slowing)
+        {
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, shipSpeed * Time.deltaTime / distance);
+            Quaternion OriginalRot = transform.rotation;
+            transform.LookAt(newPosition);
+            Quaternion NewRot = transform.rotation;
+            transform.rotation = OriginalRot;
+            transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, shipTurnSpeed * Time.deltaTime);
         }
 
     }
