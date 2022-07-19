08af6af3da377adf9922d65b1f9efd505ddab997
Forest San Filippo <freakyforest@gmail.com>
2022-07-19 15:16:39 -0500

    Added Rough Pass On Whale Fall Dream
    - Added wip models for whale fall dream (whale, creatures, spine and gravestones)\
    - Added new scenes for dream
    - Added timeline playable asset for dream
    - Added new fake scannable controller and progress animation
    - Added new specter material for vertebrae
    - Added script for triggering material swap glitch
    - Added signal asset for triggering material swap glitch and trigger a few times in timeline
    -  Added gravestone prefab with vertebrae and gravestone setup

---
+++ Assets/MaterialGlitchSwap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialGlitchSwap : MonoBehaviour
+{
+
+    public MeshRenderer meshForMatSwap;
+    public Material glitchMaterial;
+    public float glitchMin = 0.3f;
+    public float glitchMax = 0.8f;
+    private Material startingMaterial;
+    public bool glitching = false;
+    private bool matSwapped = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (meshForMatSwap)
+        {
+            startingMaterial = meshForMatSwap.material;
+        }
+    }
+
+    private void Update()
+    {
+        if (glitching)
+        {
+            if (startingMaterial && glitchMaterial)
+            {
+                StartCoroutine(MaterialGlitch());
+            }
+        }
+    }
+
+    public void StartGlitching()
+    {
+        glitching = true;
+    }
+
+    IEnumerator MaterialGlitch()
+    {
+
+        float delay = Random.Range(glitchMin, glitchMax);
+        yield return new WaitForSeconds(delay);
+        meshForMatSwap.material = glitchMaterial;
+        yield return new WaitForSeconds(delay);
+        meshForMatSwap.material = startingMaterial;
+        StartCoroutine(MaterialGlitch());
+    }
+}
