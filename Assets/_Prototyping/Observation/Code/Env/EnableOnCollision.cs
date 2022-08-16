92c9a9986bcf874dd0730d172d1a45c3b4002881
Forest San Filippo <freakyforest@gmail.com>
2022-08-16 08:10:20 -0500

    Added Urchin Barren Dream
    - Added gameplay and visuals scenes for dream (Dream04)
    - Added 2 timelines for dream
    - Added script to trigger a timeline on collision
    - Added prefabs with temp art for crashed sup, Sam's sub and Sam
    - Added terrain asset for dream
    - Added 2 post process profiles for dream
    - Set up dream in new scenes

---
+++ Assets/_Prototyping/Observation/Code/Env/EnableOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnCollision : MonoBehaviour
+{
+    public GameObject[] objectsToEnable;
+    public GameObject[] objectsToDisable;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (var enableObj in objectsToEnable)
+        {
+            enableObj.SetActive(true);
+        }
+
+        foreach (var disableObj in objectsToDisable)
+        {
+            disableObj.SetActive(false);
+        }
+    }
+
+}
