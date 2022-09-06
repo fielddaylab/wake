bec7741f1fbc8a74bbf30a834fe68ab3ee6f86da
Autumn <beauchesne@beauprime.com>
2022-09-06 11:23:15 -0500

    * Compressed streaming audio

--- Assets/StreamingAssets/compress.bat
+++ Assets/StreamingAssets/compress.bat
@@ -3,10 +3,19 @@
 set root=%cd%
 for /d /r %%s in (*) do (
     cd %%s
+    
     set processDir="%%s"
     echo processing: %processDir%
+    
     magick mogrify -resize "1024x1024>" -verbose *.png
     pngquant *.png --ext .png --verbose -f --skip-if-larger
+    
+    for %%a in (*.mp3) do (
+        ffmpeg -i "%%a" -ab 96k "temp-%%a"
+        del "%%a"
+        ren "temp-%%a" "%%a"
+    )
+    
     cd %root%
 )
 
