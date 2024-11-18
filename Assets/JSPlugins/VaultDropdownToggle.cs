ea448973a0f23fbee15c023a7731a57464a3fcbe
XanGrab <xander@xangrab.com>
2024-11-18 14:41:54 -0600

    Clean up latest changes

--- Assets/JSPlugins/VaultDropdownToggle.cs
+++ Assets/JSPlugins/VaultDropdownToggle.cs
@@ -5,22 +5,12 @@ using System.Runtime.InteropServices;
 public class VaultDropdownToggle : MonoBehaviour {
     public string sceneToDisplay = "Title";
 
-    // [DllImport("__Internal")]
-    // private static extern void EnableVaultButton();
-
     [DllImport("__Internal")]
     private static extern void DisableVaultButton();
 
     private void OnEnable() {
-        // SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
-
-    // void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-    //     if(scene.name != sceneToDisplay) return;
-
-    //     EnableVaultButton();
-    // }
 
     private void OnSceneUnloaded(Scene current) {
         if(current.name != sceneToDisplay) return;
